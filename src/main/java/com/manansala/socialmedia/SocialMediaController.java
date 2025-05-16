e19dddc2a6701268faf83ae4899ba90430bdfa39
NateDeCod3 <100500429+NateDeCod3@users.noreply.github.com>
2025-05-16 19:59:33 +0800

    Update SocialMediaController.java

--- src/main/java/com/manansala/socialmedia/SocialMediaController.java
+++ src/main/java/com/manansala/socialmedia/SocialMediaController.java
@@ -3,9 +3,7 @@ package com.manansala.socialmedia;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/manansala")
