9658c349ffb46847b71baa58e7c16c52924c7781
NateDeCod3 <100500429+NateDeCod3@users.noreply.github.com>
2025-05-16 22:43:34 +0800

    Update SocialMediaRepository.java

--- src/main/java/com/manansala/socialmedia/SocialMediaRepository.java
+++ src/main/java/com/manansala/socialmedia/SocialMediaRepository.java
@@ -1,14 +1,10 @@
 package com.manansala.socialmedia;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface SocialMediaRepository extends JpaRepository<SocialMedia, Long> {
-    Page<SocialMedia> findByTitleContainingOrDescriptionContaining(
-        String titleKeyword, 
-        String descriptionKeyword, 
-        Pageable pageable);
+    List<SocialMedia> findByTitleContainingOrDescriptionContaining(String title, String description);
 }
