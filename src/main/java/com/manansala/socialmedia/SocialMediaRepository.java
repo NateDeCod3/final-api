986ebadde67a580fd80ab5ae594b070b7e6c3126
NateDeCod3 <100500429+NateDeCod3@users.noreply.github.com>
2025-05-16 19:58:24 +0800

    Update SocialMediaRepository.java

--- src/main/java/com/manansala/socialmedia/SocialMediaRepository.java
+++ src/main/java/com/manansala/socialmedia/SocialMediaRepository.java
@@ -1,16 +1,10 @@
 package com.manansala.socialmedia;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import java.util.List;
 
-@ControllerAdvice
-public class GlobalExceptionHandler {
-    
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException(Exception e) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-            .body("Error: " + e.getMessage());
-    }
+@Repository
+public interface SocialMediaRepository extends JpaRepository<SocialMedia, Long> {
+    List<SocialMedia> findByTitleContainingOrDescriptionContaining(String title, String description);
 }
